5274871213056f2463feb95e955e6478f9c01867
balavikas113 <balavikas07@gmail.com>
2025-04-15 06:47:15 +0530

    Add files via upload

---
+++ assignment1_2.py
@@ -0,0 +1,10 @@
+first=input("enter your first name:")
+last=input("enter your last name:")
+first=str(first)
+last=str(last)
+
+a=first+last
+a=str(a)
+
+
+print("hello," ,a,"! Welcome to the python program")
